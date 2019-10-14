8d9240c671f0c5b99e4294bde308326d621a7267
Danil Kovalev <hhadevs@gmail.com>
2019-10-14 22:52:15 +0300

    Initial Commit

---
+++ Incrementor/Incrementor.h
@@ -0,0 +1,19 @@
+//
+//  Incrementor.h
+//  Incrementor
+//
+//  Created by Hadevs on 14/10/2019.
+//  Copyright © 2019 Hadevs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Incrementor.
+FOUNDATION_EXPORT double IncrementorVersionNumber;
+
+//! Project version string for Incrementor.
+FOUNDATION_EXPORT const unsigned char IncrementorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Incrementor/PublicHeader.h>
+
+
